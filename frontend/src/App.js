49aefbd0e839e27c08eaaf441efb248673f72450
Henri van der Stelt <hj.vdstelt@gmail.com>
2025-11-21 18:23:51 +0100

    styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import NavBar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </Router>
+    </>
   );
 }
 
